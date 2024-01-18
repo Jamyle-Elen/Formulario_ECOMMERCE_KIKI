b65d5dbe957936474c930901a518cebc347b637a
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-18 19:31:11 -0300

    Forms

---
+++ script.js
@@ -0,0 +1,31 @@
+class FormSubmit {
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+    }
+
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListerner("click", () => this.displaySuccess());
+        return this;
+    }
+}
+
+const FormSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    erro: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>"
+
+});
